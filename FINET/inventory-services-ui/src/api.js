126ee338403c80e6e08c56e8d9d7645f7c21c324
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2026-02-01 23:51:11 +0000

    correctional on link

--- FINET/inventory-services-ui/src/api.js
+++ FINET/inventory-services-ui/src/api.js
@@ -2,4 +2,4 @@
 import axios from 'axios';
 
 export const fetchProducts = () => 
-    axios.get("http://127.0.0.1:8000/api/products/"); //use your backend server address here
+    axios.get("http://127.0.0.1:8000/api/inventory/products/"); //use your backend server address here
