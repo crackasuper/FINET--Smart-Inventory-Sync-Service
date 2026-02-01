46b77dcbc82fa6f68dbd6dd7b976ef8a3d28f38b
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2026-02-01 20:43:25 +0000

    preparing model for products data

--- FINET/inventory_services/inventory/models.py
+++ FINET/inventory_services/inventory/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+#preparing DB models for inventory app
+
+class Product(models.Model):
+    name = models.CharField(max_length=225)
+    sku = models.CharField(max_length=100, unique=True)
+    quantity = models.IntegerField()
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} (SKU: {self.sku}) - Quantity: {self.quantity}"
